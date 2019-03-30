be02eebd68b604c639be52db5253a5ec21645cc5
Abdumuminov, Abdumuminov <umedzhon.abdumuminov@intel.com>
2019-03-30 17:46:43 +0300

    Stub for queue

--- frontend/src/app/components/TopBar.jsx
+++ frontend/src/app/components/TopBar.jsx
@@ -5,20 +5,26 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Switch from '@material-ui/core/Switch';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 
-export const TopBar = props => (
-    <AppBar>
-        <Toolbar>
-            <FormControlLabel
-                control={
-                    <Switch
-                        checked={props.showQueue}
-                        onChange={props.handleShowQueueChange()}
-                        value="showQueue"
-                    />
-                }
-                label="Secondary"
-            />
-        </Toolbar>
-    </AppBar>
-);
+// const styles = theme => ({appBarSpacer: theme.mixins.toolbar});
+
+export default (props => (
+    <div>
+        <AppBar>
+            <Toolbar>
+                <FormControlLabel
+                    control={
+                        <Switch
+                            checked={props.showQueue}
+                            onChange={props.handleShowQueueChange()}
+                            value="showQueue"
+                        />
+                    }
+                    label="Secondary"
+                />
+            </Toolbar>
+        </AppBar>
+        <div style={{height: 70}} />
+    </div>
+
+));
 
