330afc679892675056d1cc9260e49c546dc74df0
Koryun <koryun@live.com>
2019-03-30 16:26:00 +0300

    Added mongo support

--- td-server/app/__init__.py
+++ td-server/app/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask
+from flask_pymongo import PyMongo
+
 
 app = Flask(__name__)
+app.config.from_object('app.config.BaseConfig')
+mongo = PyMongo(app)
 
 from app import routes
