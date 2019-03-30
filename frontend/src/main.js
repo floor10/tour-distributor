008dc4d6a7d989d46813d4a546c0cc3d6f6d544b
Umedzhon Abdumuminov <ovegug@gmail.com>
2019-03-30 15:40:39 +0300

    Added runnable initial app

---
+++ frontend/src/main.js
@@ -0,0 +1,18 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { Provider } from 'react-redux';
+// import { BrowserRouter } from "react-router-dom";
+
+// import { App } from "app";
+// import { configureStore } from "app/store"
+
+ReactDOM.render(
+    <p>hello</p>
+//   <Provider store={configureStore()}>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </Provider>
+  ,
+  document.getElementById("root")
+);
