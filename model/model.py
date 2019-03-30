f237a56ff7928a21708d8b06b6796c80a847cdf6
Andrey Sazonov <andrey.sazonov@intel.com>
2019-03-30 21:09:36 +0300

    Add multiplicative features

--- model/model.py
+++ model/model.py
@@ -10,11 +10,16 @@
 #    * Years from last vacation [int]
 
 from enum import Enum
-
+from math import inf
+from datetime import datetime
 
 class TourType(Enum):
-    CHILD_CAMP=0
-    HEALING=1
+    CHILD=0
+    FAMILY=1
+
+    def to_str(self):
+        return self.name
+
 
 class FeatureType(Enum):
     SINGLE_PARENT=0,
@@ -23,16 +28,25 @@ class FeatureType(Enum):
     BEST_MEMBER=3,
     REWARDS=4,
     EXPERIENCE=5,
-    YEARS_FROM_LAST_TOUR=6
+    PREV_VACATIONS=6,
+    DISCIPLINARY_SANCTION=7
+
 
 FEATURE_NAME_TO_FEATURE_TYPE = {
-    "single_parent": FeatureType.SINGLE_PARENT,
-    "guardian": FeatureType.GUARDIAN,
-    "children_count": FeatureType.CHILDREN_COUNT,
-    "best_member": FeatureType.BEST_MEMBER,
-    "rewards": FeatureType.REWARDS,
-    "experience": FeatureType.EXPERIENCE
+    "additive": {
+        "single_parent": FeatureType.SINGLE_PARENT,
+        "guardian": FeatureType.GUARDIAN,
+        "children_count": FeatureType.CHILDREN_COUNT,
+        "best_member": FeatureType.BEST_MEMBER,
+        "rewards": FeatureType.REWARDS,
+        "experience": FeatureType.EXPERIENCE,
+    },
+    "multiplicative": {
+        "prev_vacations": FeatureType.PREV_VACATIONS,
+        "disciplinary_sanction": FeatureType.DISCIPLINARY_SANCTION
+    }
 }
+
 
 FEATURE_TYPE_TO_MAX_VALUE = {
     FeatureType.SINGLE_PARENT: 1,
@@ -40,33 +54,52 @@ FEATURE_TYPE_TO_MAX_VALUE = {
     FeatureType.CHILDREN_COUNT: 5,
     FeatureType.BEST_MEMBER: 2,
     FeatureType.REWARDS: 2,
-    FeatureType.EXPERIENCE: 10
+    FeatureType.EXPERIENCE: 10,
 }
 
 FEATURE_TYPE_TO_WEIGHT = {feature: 1 / max_value for feature, max_value in FEATURE_TYPE_TO_MAX_VALUE.items()}
 
 
-def extract_cropped_features(personal_info: dict, tour_type: Enum):
+def extract_additive_features(personal_info: dict, tour_type: Enum):
     out = {}
-    for feature_name in FEATURE_NAME_TO_FEATURE_TYPE:
+
+    # crop features
+    for feature_name in FEATURE_NAME_TO_FEATURE_TYPE["to_crop"]:
         if feature_name in personal_info:
             out[feature_name] = \
                 min(personal_info[feature_name], FEATURE_TYPE_TO_MAX_VALUE[FEATURE_NAME_TO_FEATURE_TYPE[feature_name]])
 
-    if tour_type == TourType.CHILD_CAMP:
+    # remove features not used for CHILD
+    if tour_type == TourType.CHILD:
         for key in ["best_member", "rewards"]:
             out.pop(key, None)
 
     return out
 
+def extract_multiplicative_features(personal_info: dict, tour_type: Enum):
+    out = {}
+
+    # parse features
+    for feature_name in FEATURE_NAME_TO_FEATURE_TYPE["multiplicative"]:
+        if feature_name in personal_info and feature_name == "prev_vacations":
+            max_date = max([datetime(vacation["date"]) for vacation in personal_info[feature_name] if vacation["type"] == tour_type.to_str()])
+            out["prev_vacation_years"] = (datetime.now() - max_date).year  # TODO: fix?
+        if feature_name in personal_info and feature_name == "disciplinary_sanction":
+            out["no_disciplinary_sanction"] = abs(1 - personal_info[feature_name])
+
+    return out
 
 def compute_score(personal_info: dict, tour_type: Enum):
     score = 0
-    features = extract_cropped_features(personal_info, tour_type)
-    for feature, val in features.items():
-        weight = FEATURE_TYPE_TO_WEIGHT[FEATURE_NAME_TO_FEATURE_TYPE[feature]]
+    additive_features = extract_additive_features(personal_info, tour_type)
+    for feature, val in additive_features.items():
+        weight = FEATURE_TYPE_TO_WEIGHT.get(FEATURE_NAME_TO_FEATURE_TYPE[feature], 0)
         score += weight * val
-    score /= len(features)
+    score /= len(additive_features)
 
-    # TODO: support multiplication feature (years from last tour of current type)
+    multiplicative_features = extract_multiplicative_features(personal_info, tour_type)
+    for feature, val in multiplicative_features.items():
+        weight = 1
+        score *= weight * val
 
+    return score
