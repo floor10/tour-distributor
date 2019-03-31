f8526a2716f58e2fd38a2cb0c996c3c09f304776
Alexey Fadeev <defident39@gmail.com>
2019-03-31 12:29:49 +0300

    Moved model to td-server

--- td-server/model/model.py
+++ td-server/model/model.py
@@ -13,23 +13,24 @@ from enum import Enum
 from math import inf
 from datetime import datetime
 
+
 class TourType(Enum):
-    CHILD=0
-    FAMILY=1
+    CHILD = 0
+    FAMILY = 1
 
     def to_str(self):
-        return self.name
+        return self.name.lower()
 
 
 class FeatureType(Enum):
-    SINGLE_PARENT=0,
-    GUARDIAN=1,
-    CHILDREN_COUNT=2,
-    BEST_MEMBER=3,
-    REWARDS=4,
-    EXPERIENCE=5,
-    PREV_VACATIONS=6,
-    DISCIPLINARY_SANCTION=7
+    SINGLE_PARENT = 0,
+    GUARDIAN = 1,
+    CHILDREN_COUNT = 2,
+    BEST_MEMBER = 3,
+    REWARDS = 4,
+    EXPERIENCE = 5,
+    PREV_VACATIONS = 6,
+    DISCIPLINARY_SANCTION = 7
 
 
 FEATURE_NAME_TO_FEATURE_TYPE = {
@@ -47,7 +48,6 @@ FEATURE_NAME_TO_FEATURE_TYPE = {
     }
 }
 
-
 FEATURE_TYPE_TO_MAX_VALUE = {
     FeatureType.SINGLE_PARENT: 1,
     FeatureType.GUARDIAN: 3,
@@ -64,10 +64,11 @@ def extract_additive_features(personal_info: dict, tour_type: Enum):
     out = {}
 
     # crop features
-    for feature_name in FEATURE_NAME_TO_FEATURE_TYPE["to_crop"]:
+    for feature_name in FEATURE_NAME_TO_FEATURE_TYPE["additive"]:
         if feature_name in personal_info:
             out[feature_name] = \
-                min(personal_info[feature_name], FEATURE_TYPE_TO_MAX_VALUE[FEATURE_NAME_TO_FEATURE_TYPE[feature_name]])
+                min(personal_info[feature_name],
+                    FEATURE_TYPE_TO_MAX_VALUE[FEATURE_NAME_TO_FEATURE_TYPE["additive"][feature_name]])
 
     # remove features not used for CHILD
     if tour_type == TourType.CHILD:
@@ -76,24 +77,29 @@ def extract_additive_features(personal_info: dict, tour_type: Enum):
 
     return out
 
+
 def extract_multiplicative_features(personal_info: dict, tour_type: Enum):
     out = {}
 
     # parse features
     for feature_name in FEATURE_NAME_TO_FEATURE_TYPE["multiplicative"]:
         if feature_name in personal_info and feature_name == "prev_vacations":
-            max_date = max([datetime(vacation["date"]) for vacation in personal_info[feature_name] if vacation["type"] == tour_type.to_str()])
-            out["prev_vacation_years"] = (datetime.now() - max_date).year  # TODO: fix?
+            max_date = max(
+                [datetime.strptime(vacation["date"], "%Y-%m-%d") for vacation in personal_info[feature_name] if
+                 vacation["type"] == tour_type.to_str()])
+            out["prev_vacation_years"] = datetime.now().year - max_date.year - \
+                                         ((datetime.now().month, datetime.now().day) < (max_date.month, max_date.day))
         if feature_name in personal_info and feature_name == "disciplinary_sanction":
             out["no_disciplinary_sanction"] = abs(1 - personal_info[feature_name])
 
     return out
 
+
 def compute_score(personal_info: dict, tour_type: Enum):
     score = 0
     additive_features = extract_additive_features(personal_info, tour_type)
     for feature, val in additive_features.items():
-        weight = FEATURE_TYPE_TO_WEIGHT.get(FEATURE_NAME_TO_FEATURE_TYPE[feature], 0)
+        weight = FEATURE_TYPE_TO_WEIGHT.get(FEATURE_NAME_TO_FEATURE_TYPE["additive"][feature], 0)
         score += weight * val
     score /= len(additive_features)
 
